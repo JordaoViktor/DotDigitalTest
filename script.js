76570683a80a09f4a8bc8878e382ac0e06243e16
Jordão Victor <jordaovpinheiro@gmail.com>
2020-08-11 23:05:08 -0300

    Getting buttons from carousel

---
+++ script.js
@@ -0,0 +1,5 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-button-right');
+const prevButton = document.querySelector('.carousel-button-left');
+console.log(nextButton)
