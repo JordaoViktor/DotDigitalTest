3e5714d2cb97434db7ffa03b916e5df01e24cad7
Jordão Victor <jordaovpinheiro@gmail.com>
2020-08-11 23:08:02 -0300

    Getting dots too!

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ const track = document.querySelector('.carousel-track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel-button-right');
 const prevButton = document.querySelector('.carousel-button-left');
+const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from(dotsNav.children);
+
+
+
 console.log(nextButton)
