52c5078aea27b1054ba68f901f4fd14cb84a22db
Jordão Victor <jordaovpinheiro@gmail.com>
2020-08-24 23:28:23 -0300

    Adding airbnb style-guide

--- src/script.js
+++ src/script.js
@@ -9,74 +9,72 @@ const dots = Array.from(dotsNav.children);
 const slidesGrid = Array.from(trackGrid.children);
 
 const slideWidthGrid = slidesGrid[0].getBoundingClientRect().width;
-const slideWidth  = slides[0].getBoundingClientRect().width;
+const slideWidth = slides[0].getBoundingClientRect().width;
 
 const trackWidth = trackGrid.offsetWidth;
 
-const mediaQuery =  1180
-const threeCards = 3
-const sizeOfmissWidth = 1.979
+const mediaQuery = 1180;
+const threeCards = 3;
+const sizeOfmissWidth = 1.979;
+const radialParse = 10;
 
-const setSlidePosition = (slide, index)=> {
-    slide.style.left = slideWidth * index + 'px';
-}
-const setSlidePositionGrid = (slide, index ) => {
-    slide.style.left =  `${slideWidthGrid  * index}px`;
-}
+const setSlidePosition = (slide, index) => {
+  slide.style.left = `${slideWidth * index}px`;
+};
+const setSlidePositionGrid = (slide, index) => {
+  slide.style.left = `${slideWidthGrid * index}px`;
+};
 
 slides.forEach(setSlidePosition);
 slidesGrid.forEach(setSlidePositionGrid);
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-    targetSlide.classList.add('current-slide');
-    track.style.transform = `translateX(-${parseInt(targetSlide.style.left)}px)`;
-    currentSlide.classList.remove('current-slide');
-}
+  targetSlide.classList.add('current-slide');
+  track.style.transform = `translateX(-${parseInt(targetSlide.style.left, radialParse)}px)`;
+  currentSlide.classList.remove('current-slide');
+};
 
 const moveToSlideGrid = (trackGrid, currentSlide, targetSlide) => {
-    const shouldReturnFalse = parseInt(targetSlide.style.left) <= slideWidthGrid * 2 + !trackWidth
-    targetSlide.classList.add('current-slide-grid');
-    if(trackWidth < mediaQuery){
-        trackGrid.style.transform = `translateX(-${parseInt(targetSlide.style.left)}px)`
+  const shouldReturnFalse = parseInt(targetSlide.style.left, radialParse) <= slideWidthGrid * 2 + !trackWidth;
+  targetSlide.classList.add('current-slide-grid');
+  if (trackWidth < mediaQuery) {
+    trackGrid.style.transform = `translateX(-${parseInt(targetSlide.style.left, radialParse)}px)`;
+  }
+  if (trackWidth >= mediaQuery) {
+    trackGrid.style.transform = `translateX(-${parseInt(targetSlide.style.left, radialParse) * threeCards}px)`;
+    if (!shouldReturnFalse) {
+      trackGrid.style.transform = `translateX(-${parseInt(trackWidth * sizeOfmissWidth, radialParse)}px)`;
+      return false;
     }
-    if(trackWidth >= mediaQuery){
-        trackGrid.style.transform = `translateX(-${parseInt(targetSlide.style.left) * threeCards}px)`;
-        if(!shouldReturnFalse){
-            trackGrid.style.transform = `translateX(-${parseInt(trackWidth * sizeOfmissWidth) }px)`
-            return false
-        }  
-    }
-    
-    
-    currentSlide.classList.remove('current-slide-grid')
-}
+  }
+  currentSlide.classList.remove('current-slide-grid');
+};
 
 const upDateDots = (currentDot, targetDot) => {
-    currentDot.classList.remove('current-slide');
-    targetDot.classList.add('current-slide');
-}
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+};
 
-prevButton.addEventListener('click', event =>{
-    const currentSlide = trackGrid.querySelector('.current-slide-grid');
-    const prevSlide = currentSlide.previousElementSibling
-     
-    moveToSlideGrid(trackGrid, currentSlide, prevSlide)    
-})
-
-nextButton.addEventListener('click', event => {
-    const currentSlide = trackGrid.querySelector('.current-slide-grid');
-    const nextSlide = currentSlide.nextElementSibling;
-
-    moveToSlideGrid(trackGrid, currentSlide, nextSlide)    
+prevButton.addEventListener('click', () => {
+  const currentSlide = trackGrid.querySelector('.current-slide-grid');
+  const prevSlide = currentSlide.previousElementSibling;
+  moveToSlideGrid(trackGrid, currentSlide, prevSlide);
 });
 
-dotsNav.addEventListener('click', event =>{
-    const targetDot = event.target.closest('button');
-    const currentSlide = track.querySelector('.current-slide');
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const targetIndex = dots.findIndex(dot => dot === targetDot);
-    const targetSlide = slides[targetIndex];
+nextButton.addEventListener('click', () => {
+  const currentSlide = trackGrid.querySelector('.current-slide-grid');
+  const nextSlide = currentSlide.nextElementSibling;
 
-    moveToSlide(track, currentSlide, targetSlide);
-    upDateDots(currentDot,targetDot);
-})
+  moveToSlideGrid(trackGrid, currentSlide, nextSlide);
+});
+
+dotsNav.addEventListener('click', (event) => {
+  const targetDot = event.target.closest('button');
+  const currentSlide = track.querySelector('.current-slide');
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlide(track, currentSlide, targetSlide);
+  upDateDots(currentDot, targetDot);
+});
